1c97ac5786973fae1de18f241a4c69f58ff5f996
ahatcher8 <drewhatcher@gmail.com>
2019-09-06 16:54:36 -0400

    Created blur and sharpen functionality

--- image-editing/editing-script.py
+++ image-editing/editing-script.py
@@ -2,19 +2,25 @@
 
 import sys
 import cv2
-import numpy
+import numpy as np
+import numpy.linalg as npla
 
 def main():
     print("Welcome to our image editor!\n")
     image_file = ask_for_image_file()
+    image_shape = None
     try:
         image = cv2.imread(image_file)
+        image_shape = image.shape
+        b,g,r = cv2.split(image)
     except:
-        #This is clearly not going to work at the moment because if the image file is some random name, the read isn't throwing an error
         print("The image file you entered was unreadable. Please ensure your image file is in the current directory, the filetype is correct, and the name is spelled properly, then rerun the script.")
         sys.exit()
-    
+
+    width = image_shape[0]
+    height = image_shape[1]
     filter_type = ask_for_filter_type()
+    matrix = None
 
     if filter_type == "blur":
         matrix = get_blur_matrix()
@@ -23,8 +29,18 @@ def main():
     else:
         print("Error: Filter Type returned no matrix.")
         sys.exit()
-    
 
+    new_b = np.zeros((width, height))
+    new_g = np.zeros((width, height))
+    new_r = np.zeros((width, height))
+
+    for i in range(1, width - 1):
+        for j in range(1, height - 1):
+            new_b[i, j] = np.sum(b[i-1:i+2, j-1:j+2] * matrix)
+            new_g[i, j] = np.sum(g[i-1:i+2, j-1:j+2] * matrix)
+            new_r[i, j] = np.sum(r[i-1:i+2, j-1:j+2] * matrix)
+    new_image = cv2.merge((new_b,new_g,new_r))
+    cv2.imwrite("new-image.jpeg", new_image)
 
 def ask_for_filter_type():
     filter_type = ""
@@ -41,11 +57,15 @@ def ask_for_image_file():
     return img_file
 
 def get_blur_matrix():
-    matrix = ''
+    matrix = [[1 / 9, 1 / 9, 1 / 9],
+              [1 / 9, 1 / 9, 1 / 9],
+              [1 / 9, 1 / 9, 1 / 9]]
     return matrix
 
 def get_sharpen_matrix():
-    matrix = ''
+    matrix = [[0, -1, 0],
+              [-1, 5, -1],
+              [0, -1, 0]]
     return matrix
 
 if __name__== "__main__":
