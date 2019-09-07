c0ccf97e35f8c544baedf800101030541d6b0df7
ahatcher8 <drewhatcher@gmail.com>
2019-09-07 00:14:41 -0400

    Clean up script and make it user friendly

--- image-editing/editing-script.py
+++ image-editing/editing-script.py
@@ -1,12 +1,12 @@
-# This will be our editing script
+# This is an image editing script that uses kernel matrices
+# to edit an images rgb values and produce a new image
 
 import sys
 import cv2
 import numpy as np
-import numpy.linalg as npla
 
 def main():
-    print("Welcome to our image editor!\n")
+    print("\nWelcome to our image editor!\n")
     image_file = ask_for_image_file()
     image_shape = None
     try:
@@ -14,7 +14,7 @@ def main():
         image_shape = image.shape
         b,g,r = cv2.split(image)
     except:
-        print("The image file you entered was unreadable. Please ensure your image file is in the current directory, the filetype is correct, and the name is spelled properly, then rerun the script.")
+        print("The image file you entered was unreadable. Please ensure your image file is in the current directory, the filetype is correct, and the name is spelled properly. Then rerun the script.")
         sys.exit()
 
     width = image_shape[0]
@@ -39,21 +39,28 @@ def main():
             new_b[i, j] = np.sum(b[i-1:i+2, j-1:j+2] * matrix)
             new_g[i, j] = np.sum(g[i-1:i+2, j-1:j+2] * matrix)
             new_r[i, j] = np.sum(r[i-1:i+2, j-1:j+2] * matrix)
+    
     new_image = cv2.merge((new_b,new_g,new_r))
-    cv2.imwrite("new-image.jpeg", new_image)
+    new_name = image_file.split('.')[0] + '-' + filter_type + '.jpg'
+    cv2.imwrite(new_name, new_image)
+    print("Your " + filter_type + " is complete!")
 
 def ask_for_filter_type():
     filter_type = ""
     while filter_type != "blur" and filter_type != "sharpen":
-        filter_type = input('How would you like to edit your image? Enter "blur" for blur, or "sharpen" for sharpen: ')
-        if filter_type != "blur" and filter_type != "sharpen":
+        filter_type = input('How would you like to edit your image? Enter "o" for options: ')
+        if filter_type == "o":
+            print('\nEnter:\n    - "blur" to blur your image\n    - "sharpen" to sharpen your image\n')
+        elif filter_type != "blur" and filter_type != "sharpen":
             print("Please retry.")
+    print("Processing... this may take a second.")
     return filter_type
 
 def ask_for_image_file():
     img_file = ""
     while img_file == "":
         img_file = input('Enter the file name of the image you would like to edit: ')
+        print('')
     return img_file
 
 def get_blur_matrix():
